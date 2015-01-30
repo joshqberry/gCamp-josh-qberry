1ba7c56cb94740f962f4369a4e91426b3cd7c71e
Josh Qberry <joshqberry@Joshs-MacBook-Air.local>
2015-01-29 23:11:57 -0700

    continues to adjust styling of new and edit pages; adds Due_Date attribute to Task class; adds field in new page to allow user to enter due date

--- db/migrate/20150126211220_create_tasks.rb
+++ db/migrate/20150126211220_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.string :description
-
+      t.date :due_date
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20150126211220) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
+    t.date     "due_date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
