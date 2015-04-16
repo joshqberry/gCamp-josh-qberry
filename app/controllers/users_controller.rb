772219f1b98d872c051a9c6872632b884fb17834
Josh Qberry <joshqberry@gmail.com>
2015-04-16 04:19:13 -0600

    brief change to user params in order to create admin user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -92,7 +92,7 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :admin)
     end
 
     def user_admin_params
